c7621b51b0d743cb2e5d430565b58f62796c8945
niket-goel <niket.goel@live.com>
2014-09-21 21:46:32 -0400

    Final files for task2

---
+++ hw1/task2/Readme.txt
@@ -0,0 +1,22 @@
+This program computes the frequency of every pair of words in a given document using map-reduce.
+The usage is the following : 
+
+============================================================================================================================================
+FutureGrid: 
+#$HADOOP_HOME/bin/hadoop --config $HADOOP_CONF_DIR jar WordCountDouble.jar wordcountdouble <input folder> <output folder>
+
+A few things to keep in mind are:
+1. Both the input and output files are assumed to be in HDFS. This means that before running the code the input files must be copied into HDFS and the ouput has to be copied out of HDFS to the local filesystem.
+2. The program will not work if the <output folder> already exists.
+
+=============================================================================================================================================
+Amazon AWS:
+After creating the cluster on EMR, you need to add a step to execute a custom jar. All the files are read from and written to an s3 bucket. The jar itself must also be placed in an s3 bucket. 
+The exact location of the custom jar must be specified in the path-to-jar.
+
+The arguments must be given as:
+wordcountdouble s3://<bucket-name>/<path-to-input> s3://<bucket-name>/<path-to-output>
+
+Again the same rules apply, i.e. the output folder MUST NOT already exist.
+
+=============================================================================================================================================== 
--- hw1/task2/wordcountdouble.java
+++ hw1/task2/wordcountdouble.java
@@ -1,13 +1,26 @@
 package org.myorg;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.Iterator;
+import java.util.StringTokenizer;
 
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.*;
-import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapred.TextInputFormat;
+import org.apache.hadoop.mapred.TextOutputFormat;
 
-public class wordcount {
+public class wordcountdouble {
 
 	public static class Map extends MapReduceBase implements
 			Mapper<LongWritable, Text, Text, IntWritable> {
@@ -18,27 +31,34 @@ public class wordcount {
 				OutputCollector<Text, IntWritable> output, Reporter reporter)
 				throws IOException {
 			String line = value.toString();
-			String firstWord;
-			String secondWord;
-			String theWord;
+			String wordOne;
+			String wordTwo;
+			String completeToken;
 			StringTokenizer tokenizer = new StringTokenizer(line);
 			try {
-				firstWord = tokenizer.nextToken();
+				/*
+				 * The following code increments the tokenizer twice and also
+				 * sets completeToken as the concatenation of two words that are
+				 * separated by a " " delimiter. The mapper passes these
+				 * concatenated words as the key to the reducers
+				 */
+				wordOne = tokenizer.nextToken();
 				while (tokenizer.hasMoreTokens()) {
-					
-					if(tokenizer.hasMoreElements()) {
-						secondWord = tokenizer.nextToken();
+
+					if (tokenizer.hasMoreElements()) {
+						wordTwo = tokenizer.nextToken();
 					} else {
-						secondWord = "";
+						wordTwo = "";
 					}
-					theWord = firstWord + " " + secondWord;
-					firstWord = secondWord;
-					word.set(theWord);
+					completeToken = wordOne + " " + wordTwo;
+					wordOne = wordTwo;
+					word.set(completeToken);
 					output.collect(word, one);
 				}
-			}
-			catch (Exception e) {
-				System.err.print("An exception occurred. Probably the document was empty :::"+e.getMessage());
+			} catch (Exception e) {
+				System.err
+						.print("An exception occurred. Probably the document was empty :::"
+								+ e.getMessage());
 			}
 		}
 	}
@@ -48,6 +68,7 @@ public class wordcount {
 		public void reduce(Text key, Iterator<IntWritable> values,
 				OutputCollector<Text, IntWritable> output, Reporter reporter)
 				throws IOException {
+			// The reducer adds up the occurrences of a particular word.
 			int sum = 0;
 			while (values.hasNext()) {
 				sum += values.next().get();
@@ -57,7 +78,8 @@ public class wordcount {
 	}
 
 	public static void main(String[] args) throws Exception {
-		JobConf conf = new JobConf(wordcount.class);
+		// Configuring the Hadoop job parameters
+		JobConf conf = new JobConf(WordCount.class);
 		conf.setJobName("wordcount");
 
 		conf.setOutputKeyClass(Text.class);
@@ -69,6 +91,11 @@ public class wordcount {
 
 		conf.setInputFormat(TextInputFormat.class);
 		conf.setOutputFormat(TextOutputFormat.class);
+
+		/*
+		 * Reading input parameters. The parameters used are args[1] and args[2]
+		 * as the class name itself is one parameter for the jar
+		 */
 
 		FileInputFormat.setInputPaths(conf, new Path(args[1]));
 		FileOutputFormat.setOutputPath(conf, new Path(args[2]));
